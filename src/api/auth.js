261a305cfc0a757e605539abaa947d871c87ebdc
HO <huynhanhpham734@gmail.com>
2022-12-14 14:12:22 +0700

    install toast

---
+++ src/api/auth.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+const path = "auth/";
+
+export const authAPI = {
+   async login(payload) {
+      return await axios.post(path + "login", payload);
+   },
+
+   async register(payload) {
+      return await axios.post(path + "register", payload);
+   },
+
+   async me() {
+      return await axios.post(path + "me");
+   },
+};
