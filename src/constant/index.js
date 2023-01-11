81651c5126a29a2a6dc6c42a9655ac64f45bb742
HO <huynhanhpham734@gmail.com>
2023-01-11 23:31:20 +0700

    deploy

--- src/constant/index.js
+++ src/constant/index.js
@@ -1,4 +1,5 @@
-export const api = "http://localhost:3000/";
+// export const api = "http://localhost:3000/";
+export const api = "https://manager-company-be-production.up.railway.app/";
 export const alphaNumericPattern = /^[a-zA-Z0-9_ .-]*$/;
 export const emailrgx =
    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,6}))$/;
