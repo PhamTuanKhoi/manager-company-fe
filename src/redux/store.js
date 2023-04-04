8ff2552bf41991411c93d20ae712811453dde513
huynhanhpham <huynhanhpham734@gmail.com>
2023-04-04 09:04:15 +0700

    fix preloadstate

--- src/redux/store.js
+++ src/redux/store.js
@@ -50,13 +50,16 @@ const reducer = combineReducers({
 
 const persistedReducer = persistReducer(persistConfig, reducer);
 
-const store = configureStore({
-   reducer: persistedReducer,
-   middleware: (getDefaultMiddleware) =>
-      getDefaultMiddleware({
-         serializableCheck: false,
-      }),
-});
+const store = configureStore(
+   {
+      reducer: persistedReducer,
+      middleware: (getDefaultMiddleware) =>
+         getDefaultMiddleware({
+            serializableCheck: false,
+         }),
+   },
+   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 export const persistor = persistStore(store);
 export default store;
