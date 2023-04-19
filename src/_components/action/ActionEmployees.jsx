403456f56ee1f72fee01e6fc9a907af659d5fe50
huynhanhpham <huynhanhpham734@gmail.com>
2023-04-19 17:01:48 +0700

    move and fix filter api employees

---
+++ src/_components/action/ActionEmployees.jsx
@@ -0,0 +1,25 @@
+import React, { memo, useState } from "react";
+
+function ActionEmployees({ department, setDepartment, departments }) {
+   return (
+      <div className="col-sm-6 col-md-3">
+         <div className="form-group form-focus select-focus">
+            <select
+               className="form-control floating"
+               value={department}
+               onChange={(e) => setDepartment(e.target.value)}
+            >
+               <option value={""}>Tất cả</option>
+               {departments?.map((item) => (
+                  <option key={item?._id} value={item?._id}>
+                     {item?.name}
+                  </option>
+               ))}
+            </select>
+            <label className="focus-label">Vị trí</label>
+         </div>
+      </div>
+   );
+}
+
+export default memo(ActionEmployees);
